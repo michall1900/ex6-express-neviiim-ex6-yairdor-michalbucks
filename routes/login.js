0390fcc4082a6f60e661202af0c03d3b322d1ebc
Michal Bucks <michall1900@gmail.com>
2023-01-30 23:34:52 +0200

    Delete unnessesary function

--- routes/login.js
+++ routes/login.js
@@ -6,8 +6,5 @@ const db = require("../models")
 router.get('/',users.getLoginPage)
 router.post('/', users.postLogin)
 
-router.get("/db",(req,res)=>{
-    db.User.findAll().then((j)=>res.json(j))
-})
 
 module.exports = router;
